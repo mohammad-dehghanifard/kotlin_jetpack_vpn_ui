aaf6bd0fd09e864a3d14648851f43077fde82d45
mohammad <m.deheghanifard@gmail.com>
2024-03-02 13:33:10 +0330

    impl ConnectionStatus eum for state management

--- app/src/main/java/ir/dehghanifard/kotlin_jetpack_vpn_ui/MainActivity.kt
+++ app/src/main/java/ir/dehghanifard/kotlin_jetpack_vpn_ui/MainActivity.kt
@@ -7,14 +7,22 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import ir.dehghanifard.kotlin_jetpack_vpn_ui.components.AppBarOutLineButton
@@ -54,7 +62,37 @@ fun MainView() {
         Column(modifier = Modifier
             .fillMaxSize()
             .padding(25.dp),) {
+            // app bar
             AppTopAppBar()
+            Spacer(modifier = Modifier.height(20.dp) )
+            // power key
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(280.dp),
+            ) {
+                Icon(
+                    painterResource(id = R.drawable.circle),
+                    contentDescription = null ,
+                    modifier = Modifier
+                        .size(250.dp)
+                        .align(Alignment.Center)
+                )
+
+                IconButton(
+                    onClick = { /*TODO*/ },
+                    modifier = Modifier
+                        .size(120.dp)
+                        .align(Alignment.Center)
+                ) {
+                    Icon(
+                        painterResource(id = R.drawable.power),
+                        contentDescription = null ,
+                        tint = Color.White,
+
+                    )
+                }
+            }
         }
 
     }
---
+++ app/src/main/java/ir/dehghanifard/kotlin_jetpack_vpn_ui/helpers/ConnectioStatus.kt
@@ -0,0 +1,7 @@
+package ir.dehghanifard.kotlin_jetpack_vpn_ui.helpers
+
+enum class ConnectionStatus {
+    DisConnect,
+    Connecting,
+    Connected
+}
